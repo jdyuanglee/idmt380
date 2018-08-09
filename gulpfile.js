bdce433b7aac5bd47e45c0f608b5e17607a01641
axelmortimer <axelmortimer@gmail.com>
2018-08-09 19:47:06 -0400

    Added gulp functionality

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+
+gulp.task('html', () => {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('css', () => {
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('js', () => {
+  return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('img', () => {
+  return gulp.src('src/img/')
+    .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('default', ['html','css','js','img']);
