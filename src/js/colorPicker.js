7e4d862753776c910ad6bea77ceb0a3afc15da68
axelmortimer <axelmortimer@gmail.com>
2018-08-12 21:57:59 -0400

    fixed color picker and svg implementation

--- src/js/colorPicker.js
+++ src/js/colorPicker.js
@@ -1,12 +1,16 @@
 var style;
 var chosenColor;
+var chosenGroup;
 
 function picked(button) {
   style = window.getComputedStyle(button);
   chosenColor = style.backgroundColor;
+  // console.log($('g g:hover path').css(fill));
+  // $('g g:hover path').css('fill', chosenColor);
   document.getElementsByClassName('picker-indicator')[0].style.backgroundColor = style.backgroundColor;
 }
 
-$('path').on('click', function() {
-    $(this).css('fill', chosenColor);
+$('g g').on('click', function() {
+  chosenGroup = '#' + this.id + ' *';
+  $(chosenGroup).css('fill', chosenColor);
 });
