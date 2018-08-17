8ab4c33f85a1c7c672bf006090d689c77f1a9b68
axelmortimer <axelmortimer@gmail.com>
2018-08-16 21:40:55 -0400

    Updated gulpfile with minify and map modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 
 gulp.task('html', () => {
+  del(['./dist/*.html']);
   return gulp.src('src/*.html')
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('css', () => {
+  del(['./dist/css/*.+(css|map)']);
   return gulp.src('src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('js', () => {
+  del(['./dist/js/*.+(js|map)']);
   return gulp.src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('img', () => {
+  del(['./dist/img/*']);
   return gulp.src('src/img/*')
     .pipe(gulp.dest('dist/img/'));
 });
